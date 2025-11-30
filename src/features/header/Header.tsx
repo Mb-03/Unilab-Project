37ee1e9d7194702e0879784cbbfedd90a84812ab
Mb-03 <baratashvilimisho@gmail.com>
2025-11-30 15:37:59 +0400

    initial

---
+++ src/features/header/Header.tsx
@@ -0,0 +1,14 @@
+import { Logo } from "."
+
+
+const Header = () => {
+  return (
+    <div>
+        <header>
+            <img src={Logo} alt="" />
+        </header>
+    </div>
+  )
+}
+
+export default Header
